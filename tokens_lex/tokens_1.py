babd49803bbb4232cef47257927d42c3c835dc52
steffanyvallejo <stefyvallejo.vallejo@gmail.com>
2018-11-12 23:32:55 -0500

    Declaraciones y asignaciones de variables

--- tokens_lex/tokens_1.py
+++ tokens_lex/tokens_1.py
@@ -53,6 +53,7 @@ t_EXPONENT = r'\*\*'
 t_DIVIDEINT = r'//'
 t_MODULE = r'%'
 
+
 #Para numeros
 def t_NUMBER(t):
     r'\d+'
@@ -76,29 +77,95 @@ def t_error(t):
 
 lexer = lex.lex()
 
+
+#Reglas de parsing
+precedence = (
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS'),
+    )
+
+#Diccionario de nombres
+names = { }
+
+def p_statement_assign(t):
+    'statement : NAME EQUALS expression'
+    names[t[1]] = t[3]
+
+def p_statement_expr(t):
+    'statement : expression'
+    print(t[1])
+
+def p_expression_binop(t):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if t[2] == '+'  : t[0] = t[1] + t[3]
+    elif t[2] == '-': t[0] = t[1] - t[3]
+    elif t[2] == '*': t[0] = t[1] * t[3]
+    elif t[2] == '/': t[0] = t[1] / t[3]
+
+
+def p_expression_uminus(t):
+    'expression : MINUS expression %prec UMINUS'
+    t[0] = -t[2]
+
+def p_expression_group(t):
+    'expression : LPAREN expression RPAREN'
+    t[0] = t[2]
+
+def p_expression_number(t):
+    'expression : NUMBER'
+    t[0] = t[1]
+
+def p_expression_name(t):
+    'expression : NAME'
+    try:
+        t[0] = names[t[1]]
+    except LookupError:
+        print("Undefined name '%s'" % t[1])
+        t[0] = 0
+
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
 #Pruebas
-cadena1 = "3 + 4 * 10 + -20 *2"
-cadena2 = "x = 3 * 4 + 5 * 6"
-cadena3 = "for = 3 * 4 + 5 * 6"
-cadena4 = "x == True"
-cadena5 = "x <> 10"
-cadena6 = "x and y"
-cadena7 = "x**10"
-cadena8 = "x//2"
-cadena9 = "10%2"
+#cadena1 = "3 + 4 * 10 + -20 *2"
+#cadena2 = "x = 3 * 4 + 5 * 6"
+#cadena3 = "for = 3 * 4 + 5 * 6"
+#cadena4 = "x == True"
+#cadena5 = "x <> 10"
+#cadena6 = "x and y"
+#cadena7 = "x**10"
+#cadena8 = "x//2"
+#cadena9 = "10%2"
 
 #lexer.input(cadena1)
 #lexer.input(cadena2)
 #lexer.input(cadena3)
 #lexer.input(cadena4)
-lexer.input(cadena5)
+#lexer.input(cadena5)
 #lexer.input(cadena6)
 #lexer.input(cadena7)
 #lexer.input(cadena8)
 #lexer.input(cadena9)
 
-while True:
-    tok = lexer.token()
-    if not tok: break
-    print(tok)
+#while True:
+ #   tok = lexer.token()
+  #  if not tok: break
+   # print(tok)
     #Formato de la salida: LexRoken(type, value, lineno, lexpos)
+
+
+while True:
+    try:
+        s = input('>> ')   # Use raw_input on Python 2
+    except EOFError:
+        break
+    resultado=parser.parse(s)
+    print(resultado)
+
