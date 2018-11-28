dece0b99ccd097488296f34ffb3a57448c062b4e
Steffany Vallejo <stefyvallejo.vallejo@gmail.com>
2018-11-27 21:54:00 -0500

    Interfaz

--- tokens_lex/Interfaz.py
+++ tokens_lex/Interfaz.py
@@ -2,71 +2,39 @@ import tkinter.messagebox
 from tkinter import *
 from tkinter.ttk import Frame, Label, Entry
 
+root=Tk()
+root.geometry("800x600")
+def retrieve_input():
+    inputValue=text1.get("1.0","end-1c")
+    print(inputValue)
 
 
-class App(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        self.initUI()
+btnSint=Button(root, height=2, width=14, text="Analisis sintactico", bg="green",
+                    command=lambda: retrieve_input())
+#command=lambda: retrieve_input() >>> just means do this when i press the button
+btnSint.pack(side=TOP, pady=2 )
 
-    def initUI(self):
-        self.parent.title("Validator Project")
-        self.pack(fill=BOTH, expand=True)
-        global value
-        value = 0
-        global expr
-        expr = StringVar()
-        global res
-        res = StringVar()
+btnLex=Button(root, height=2, width=14, text="Analisis lexico",bg="green",
+                    command=lambda: retrieve_input())
+#command=lambda: retrieve_input() >>> just means do this when i press the button
+btnLex.pack(side=TOP, pady=2 )
 
-        frame1 = Frame(self)
-        frame1.pack(fill=X)
-
-        lbl1 = Label(frame1, text="BIENVENIDO", width=18)
-        lbl1.pack(side=TOP, padx=5, pady=5)
+btnPlagio=Button(root, height=2, width=14, text="Plagio",bg="green",
+                    command=lambda: retrieve_input())
+#command=lambda: retrieve_input() >>> just means do this when i press the button
+btnPlagio.pack(side=TOP, pady=2)
 
 
+text1=Text(root, height=30, width=40)
+text1.pack(padx=10, side=LEFT )
 
-        frame3 = Frame(self)
-        frame3.pack(fill=Y)
+text2=Text(root, height=30, width=40)
+text2.pack(padx=0, side=LEFT)
 
-        btnSint = Button(frame3, text="Analisis Sintatico", width=15, command=self.validar, bg = "green")
-        btnSint.pack(side=TOP, anchor=N, padx=8, pady=5)
-        btnLex = Button(frame3, text="Analisis Lexico", width=15, command=self.validar, bg="green")
-        btnLex.pack(side=TOP, anchor=N, padx=8, pady=5)
-        btnPlagio = Button(frame3, text="Calcular plagio", width=15, command=self.validar, bg="green")
-        btnPlagio.pack(side=TOP, anchor=N, padx=8, pady=5)
-
-        programa1 = Text(frame3, width=40, height=30)
-        programa1.pack(side=LEFT, anchor=N, padx=8, pady=5)
-        programa2 = Text(frame3, width=40, height=30)
-        programa2.pack(side=LEFT, anchor=N, padx=8, pady=5)
-
-        frame2 = Frame(self)
-        frame2.pack(fill=Y)
-
-        lbl2 = Label(frame2, text="Porcentaje de plagio es: ", width=18)
-        lbl2.pack(side=LEFT, anchor=N, padx=8, pady=5)
-        plagio = Label(frame2, text="----", width=18)
-        plagio.pack(side=LEFT, anchor=N, padx=8, pady=5)
+resul=Text(root, height=20, width=20)
+resul.pack(padx=10, side=LEFT )
 
 
-
-    def errorMsg(self,msg):
-        if msg == 'error':
-            tkinter.messagebox.showerror('Error','Ingrese expresion')
-
-    def validar(self):
-        if expr.get() == '':
-            self.errorMsg('error')
-
-def main():
-    root = Tk()
-    root.geometry("700x700")
-    app = App(root)
-    root.mainloop()
+mainloop()
 
 
-if __name__ == '__main__':
-    main()
